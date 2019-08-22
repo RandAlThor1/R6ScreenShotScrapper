9b80ab60d8cd8b9aaf4b8e91ed067cab6a233afd
RandAlThor1 <benjaminallen1@hotmail.com>
2019-08-22 08:58:02 -0500

    help me dear lord this code

--- r6statsHandler.js
+++ r6statsHandler.js
@@ -14,112 +14,120 @@ const ops = "operators";
 const wepCat = "weapon-catagories";
 const wep = "wepons";
 
-
-
 function ServiceResponseError(message) {
-    this.name = "ServiceResponseError";
-    this.message = message || "";
+  this.name = "ServiceResponseError";
+  this.message = message || "";
 }
 ServiceResponseError.prototype = Error.prototype;
 
 class r6statsHandler {
-<<<<<<< HEAD
+  constructor() {
+    this.authenticating = true;
+    client = new R6Stats({
+      username: "*********",
+      password: "***********",
+      user_agent: "R6Stats Node API Test Application"
+    });
+  }
 
+  getOtherStats(username) {
+    return this.req(username, gen)
+      .then(res => {
+        return Promise.resolve(res);
+      })
+      .catch(err => {
+        throw new ServiceResponseError(err);
+      });
+  }
 
-    getplayerElo(username) {
-        return this.req(username, seas).then(res => {
-            return Promise.resolve(res);
-        }).catch(err => {
-            throw new ServiceResponseError(err);
-=======
-    constructor() {
-        this.authenticating = true;
-        client = new R6Stats({
-            username: "*********",
-            password: "***********",
-            user_agent: "R6Stats Node API Test Application"
->>>>>>> 92d1032acf385c0a8cb57d1688365db94c01424b
-        });
+  async getPlayer(username) {
+    try {
+      let genStats = await this.getOtherStats(username);
+      let eloStats = await this.getplayerElo(username);
+      let player = {
+        username: genStats.username,
+        wlr: genStats.stats.queue.ranked.wl,
+        kdr: genStats.stats.queue.ranked.kd,
+        level: genStats.progression.level,
+        elo: eloStats.seasons.burnt_horizon.regions.ncsa[0].mmr
+      };
+      return player;
+    } catch (err) {
+      throw new ServiceResponseError(err);
     }
+  }
 
-    getOtherStats(username) {
-        return this.req(username, gen).then(res => {
-            return Promise.resolve(res);
-        }).catch(err => {
-            throw new ServiceResponseError(err);
-        });
-    }
+  req(username, type) {
+    const url = `https://api2.r6stats.com/public-api/stats/${username}/pc/${type}`;
+    var defaults = {
+      method: "GET",
+      json: true,
+      url: url,
+      headers: {
+        Authorization: "9d481f24-292e-478f-895a-34cff29736d4"
+      }
+    };
+    var options = Object.assign({}, defaults);
 
-    async getPlayer(username) {
-        try {
-            let genStats = await this.getOtherStats(username);
-            let eloStats = await this.getplayerElo(username);
-            let player = {
-                username: genStats.username,
-                wlr: genStats.stats.queue.ranked.wl,
-                kdr: genStats.stats.queue.ranked.kd,
-                level: genStats.progression.level,
-                elo: eloStats.seasons.burnt_horizon.regions.ncsa[0].mmr
-            };
-            return player;
-        } catch (err) {
-            throw new ServiceResponseError(err);
+    return rp(options);
+  }
+
+  getplayerId(num, names) {
+    this.getPlayer(names[num])
+      .then(function(player) {
+        //statsDebug(player.username, ": getPlayerId")
+        statsDebug(player.username, " : ", names[num]);
+        // statsDebug("debug: ", player.username, " : ", player.stats.ranked.wlr, " : ", player.stats.ranked.kd, " : ", player.stats.progression.level, " : ", num)
+        statsDebug(
+          player.username,
+          ": ",
+          player.wlr,
+          ": ",
+          player.kdr,
+          ": ",
+          player.level,
+          ": ",
+          player.elo,
+          ": ",
+          num
+        );
+        TH.addPlayer(
+          new user(
+            player.username,
+            player.wlr,
+            player.kdr,
+            player.level,
+            player.elo,
+            num
+          )
+        );
+        recived++;
+      })
+      .catch(error => {
+        if (error.name === "ServiceResponseError") {
+          recived++;
+          statsDebug("could not find : ", names[num]);
+        } else {
+          recived++;
+          statsDebug(error.message, " : ", error.name);
         }
-    }
-
-
-    req(username, type) {
-        const url = `https://api2.r6stats.com/public-api/stats/${username}/pc/${type}`;
-        var defaults = {
-            method: "GET",
-            json: true,
-            url: url,
-            headers: {
-                Authorization: "9d481f24-292e-478f-895a-34cff29736d4"
-            }
-        };
-        var options = Object.assign({}, defaults);
-
-        return rp(options);
-    }
-
-    getplayerId(num, names) {
-        this.getPlayer(names[num])
-            .then(function (player) {
-                //statsDebug(player.username, ": getPlayerId")
-                statsDebug(player.username, " : ", names[num]);
-                // statsDebug("debug: ", player.username, " : ", player.stats.ranked.wlr, " : ", player.stats.ranked.kd, " : ", player.stats.progression.level, " : ", num)
-                statsDebug(player.username, ": ", player.wlr, ": ", player.kdr, ": ", player.level, ": ", player.elo, ": ", num);
-                TH.addPlayer(new user(player.username, player.wlr, player.kdr, player.level, player.elo, num));
-                recived++;
-
-            })
-            .catch((error) => {
-                if (error.name === "ServiceResponseError") {
-                    recived++;
-                    statsDebug("could not find : ", names[num]);
-                }
-                else {
-                    recived++;
-                    statsDebug(error.message, " : ", error.name);
-                }
-            }).then(() => {
-                if (recived === 10) {
-                    recived = 0;
-                    this.printStats();
-                }
-            });
+      })
+      .then(() => {
         if (recived === 10) {
-            recived = 0;
-            console.log("10: ", num);
-            this.printStats();
+          recived = 0;
+          this.printStats();
         }
+      });
+    if (recived === 10) {
+      recived = 0;
+      console.log("10: ", num);
+      this.printStats();
     }
+  }
 
-
-    printStats() {
-        TH.printAverages();
-    }
+  printStats() {
+    TH.printAverages();
+  }
 }
 
 module.exports = r6statsHandler;
